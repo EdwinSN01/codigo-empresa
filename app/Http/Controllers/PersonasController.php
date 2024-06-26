15e1285da68fdf0d8b327c9fa0cd2d87598a6c3b
EdwinSN01 <edserin@unitru.edu.pe>
2024-06-25 22:00:48 -0500

    TAREA ACTUALIZAR-ELIMINAR

---
+++ app/Http/Controllers/PersonasController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Persona;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\CreatePersonaRequest;
+class PersonasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $personas = Persona::all();
+        $personas = Persona::paginate(2);
+        return view('personas',compact('personas'));
+    }
+
+    public function show($id) 
+    {
+        //return Persona::find($nPerCodigo);
+        //return view('personas.show', compact('persona'));
+
+        $persona = Persona::find($id)??new Persona();
+        return view('show', [
+            'persona' => $persona
+        ]);
+    } 
+
+     public function create(){
+
+        return view('create');
+     }
+
+     public function store(CreatePersonaRequest $request)
+     {
+         // Validación de datos (puedes personalizar esto según tus necesidades)
+         date_default_timezone_set('America/Lima');
+
+         $token = Str::random(32);
+         $persona = new Persona();
+         $persona->nPerCodigo = $request->input('nPerCodigo');
+         $persona->cPerApellido = $request->input('cPerApellido');
+         $persona->cPerNombre = $request->input('cPerNombre');
+         $persona->cPerDireccion = $request->input('cPerDireccion');
+         $persona->cPerAFecNac = $request->input('cPerAFecNac');
+         $persona->cPerEdad = $request->input('cPerEdad');
+         $persona->cPerSexo = $request->input('cPerSexo');
+         $persona->cPerSueldo = $request->input('cPerSueldo');
+         $persona->cPerRnd = $request->input('cPerRnd');
+         $persona->cPerEstado = $request->input('cPerEstado');
+         $persona->remenber_toker = $token;
+
+         $persona->save();
+
+        return redirect()->route('personas')->with('success', 'Nuevo Ingreso creado exitosamente.');
+
+} 
+
+
+public function edit(Persona $id)
+{
+
+            return view('edit',[
+                'persona'=> $id]);
+
+
+    }
+    public function update(CreatePersonaRequest $request, $id)
+    {   
+        date_default_timezone_set('America/Lima');
+        $persona = Persona::findOrFail($id);
+
+        // Actualizar los datos del formulario
+        $persona->nPerCodigo = $request->input('nPerCodigo');
+        $persona->cPerApellido = $request->input('cPerApellido');
+        $persona->cPerNombre = $request->input('cPerNombre');
+        $persona->cPerDireccion = $request->input('cPerDireccion');
+        $persona->cPerAFecNac = $request->input('cPerAFecNac');
+        $persona->cPerEdad = $request->input('cPerEdad');
+        $persona->cPerSexo = $request->input('cPerSexo');
+        $persona->cPerSueldo = $request->input('cPerSueldo');
+        $persona->cPerRnd = $request->input('cPerRnd');
+        $persona->cPerEstado = $request->input('cPerEstado');
+        $persona->remenber_toker = $request->input('remenber_toker');
+        // Actualiza más campos según sea necesario
+        $persona->save();
+
+
+
+        return redirect()->route('personas')->with('success', 'Persona creada exitosamente.');
+    }
+    public function destroy(Persona $persona) {
+        $persona->delete();
+        return redirect()->route('personas');
+
+    }
+}
